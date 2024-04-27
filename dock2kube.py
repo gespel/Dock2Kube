71fec021e556cd15a29b96cd886c7e0524574762
gespel <heimbrodt@uni-potsdam.de>
2024-04-27 11:32:53 +0200

    added requirements

--- dock2kube.py
+++ dock2kube.py
@@ -1,4 +1,21 @@
+import uuid
 import docker
 
+path = input("Enter filepath (default = current folder): ")
+if not path:
+    path = "."
+
+version = input("Enter a version number (default = 1.0.0): ")
+if not version:
+    version = "1.0.0"
+
+name = input("Enter a name (default = random uid): ")
+if not name:
+    name = uuid.UUID()
+
 client = docker.from_env()
-client.images.build(path = ".", tag = "test:1.0.0")
+(a, b) = client.images.build(path = path, tag = f"{name}:{version}")
+f = open(f"./{name}.tar", "wb")
+for c in a.save():
+    f.write(c)
+f.close()
